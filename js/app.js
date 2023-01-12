5e125a486dc480e0ddc4e78d0fc5fdddcd220731
Charles Fouquet <fouquetcharles@gmail.com>
2023-01-12 13:52:33 +0100

    Push du 20230111

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+       $("header").css("background-color","white");
+    } 
+    if ($(this).scrollTop() < 150) {
+        $("header").css("background-color","unset");
+    } 
+ });
