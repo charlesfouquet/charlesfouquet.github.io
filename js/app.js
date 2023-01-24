c95e6701c3d133862d8495e0eebbcaa6cc1577e6
Charles Fouquet <fouquetcharles@gmail.com>
2023-01-24 23:00:31 +0100

    Animation du dropdown mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
 AOS.init();
 
-$('.burgerBtn').on('click', function() {
-  $(this).toggleClass('active not-active');
+$(document).on("click", function () {
+  if ($(".burgerBtn").hasClass("active")) {
+    $(".burgerBtn").toggleClass("active not-active");
+  }
+  if ($("nav ul").hasClass("navClicked")) {
+    $("nav ul").toggleClass("navClicked");
+  }
+})
+
+$(".burgerBtn").on("click", function(e) {
+  e.stopPropagation();
+  $(this).toggleClass("active not-active");
+  $("nav ul").toggleClass("navClicked");
 });
 
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 150) {
-       $("header").css("background-color","white");
-    } 
-    if ($(this).scrollTop() < 150) {
-        $("header").css("background-color","unset");
-    } 
- });
+  if ($(this).scrollTop() > 150) {
+    $("header").css("background-color","white");
+    $("nav ul").css("background-color","white");
+    $("nav ul").css("box-shadow","0px 0px 5px rgb(91, 110, 91)");
+  } 
+  if ($(this).scrollTop() < 150) {
+    $("header").css("background-color","unset");
+    $("nav ul").css("background-color","unset");
+    $("nav ul").css("box-shadow","none");
+  } 
+});
+
+$(window).on("load", function () {
+  if ($(this).width() > 770) {
+    $("#contactLink").addClass("button");
+  } 
+  if ($(this).width() < 770) {
+    $("#contactLink").removeClass("button");
+  } 
+})
+
+$(window).on("resize", function () {
+  if ($(this).width() > 770) {
+    $("#contactLink").addClass("button");
+  } 
+  if ($(this).width() < 770) {
+    $("#contactLink").removeClass("button");
+  } 
+})
